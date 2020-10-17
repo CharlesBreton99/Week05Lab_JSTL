fba3bc064e33fb27252a4316368024ff438ceffa
Charles Breton <charlesbreton99@gmail.com>
2020-10-17 15:55:45 -0600

    Initial commit Almost all done Bug with the delete function not getting pickup when using getParameter()

---
+++ src/java/servlets/ShoppingListServlet.java
@@ -0,0 +1,61 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ShoppingListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+
+        String action = request.getParameter("action");
+        log(action);
+        log(action);
+        if (action != null && action.equals("register")) {
+            String username = request.getParameter("username");
+            session.setAttribute("name", username);
+
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
+        } else if (action != null && action.equals("add")) {
+            ArrayList items = (ArrayList) session.getAttribute("items");
+
+            if (items == null) {
+                items = new ArrayList<>();
+            }
+
+            String input = request.getParameter("item");
+            
+            items.add(input);
+            session.setAttribute("items", items);
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
+        } else if (action != null && action.equals("delete")) {
+            // response.setContentType("text/html;charset=UTF-8");
+           String itemSelected = request.getParameter("anItem");
+           log("Record is " + itemSelected);
+           
+           getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
+        } else if (action != null && action.equals("logout")) {
+
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        doGet(request, response);
+
+    }
+
+}
---
+++ web/WEB-INF/shoppinglist.jsp
@@ -0,0 +1,36 @@
+<%-- 
+    Document   : shoppinglist
+    Created on : Oct 17, 2020, 1:41:52 PM
+    Author     : Birdd
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Shopping List</title>
+    </head>
+    <body>
+        <h1>Shopping List</h1>
+        <p>Hello ${name}</p>
+
+        <form action="" method="POST">
+            <input type="text" name="item"><input type="submit" value="Add Item">
+            <input type="hidden" name="action" value='add'>
+        </form>
+        
+        <form action="" method="post">
+        <c:forEach items="${items}" var="items">
+     
+            <input type="radio" name='anItem' values='${items}'>${items} <br>
+
+        </c:forEach>       
+            <input type="submit" value="Delete">
+            <input type="hidden" name="action" value="delete">
+        </form>
+        <p><a href="ShoppingList?action="</a>Logout</p>
+    </body>
+</html>
